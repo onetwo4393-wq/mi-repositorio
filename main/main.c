cf9dec54bd4ae6c6b727d0fef8b8979970fb3e6f
limon <onetwo4393@gmail.com>
2026-03-24 00:19:47 -0300

    Update: Arquitectura BLE funcional para v6.1

---
+++ main/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+
+static const char* TAG = "KERNEL_MASTER_BLE";
+
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = 0x0020,
+    .adv_int_max        = 0x0040,
+    .adv_type           = ADV_TYPE_NONCONN_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy  = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
+    switch (event) {
+        case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:
+            esp_ble_gap_start_advertising(&adv_params);
+            break;
+        case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+            if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+                ESP_LOGE(TAG, "Fallo al iniciar: %d", param->adv_start_cmpl.status);
+            } else {
+                ESP_LOGI(TAG, ">>> NODO EMITIENDO <<<");
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
+    esp_ble_gap_set_device_name("ARQUITECTO_NODEMCU");
+    uint8_t adv_data[] = { 0x02, 0x01, 0x06, 0x03, 0x03, 0xF1, 0xFF }; 
+    esp_ble_gap_config_adv_data_raw(adv_data, sizeof(adv_data));
+}
